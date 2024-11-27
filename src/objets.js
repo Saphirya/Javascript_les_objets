29eb1b6fda10f5c1abbb3c6947153834fd8989e7
Florian <floriangeorges1992@gmail.com>
2024-11-27 11:57:12 +0100

    add objets.js

--- src/objets.js
+++ src/objets.js
@@ -1,1 +1,36 @@
-console.log("bonjour");
+// Déclaration d'un objet
+const x = {};
+console.log(x);
+
+const y = Object();
+console.log(y);
+
+const z = new Object();
+console.log(z);
+
+//propriétés
+
+const terre = {
+  satellite: "Lune",
+  population: 7e7,
+  temperature: {
+    min: -70,
+    max: 60,
+  },
+  isOld: false,
+  getTemperature: function () {
+    console.log("15");
+  },
+};
+//aucune spécificité dans l'ordre déclaré !
+console.log(terre["satellite"]);
+console.log(terre.population);
+
+const prop = "temperature";
+console.log(terre[prop]);
+
+const copy = terre;
+copy.isOld = true;
+console.log(terre);
+
+terre.getTemperature();
